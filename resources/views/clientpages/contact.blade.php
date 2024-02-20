5ee2917a11ee0f31e0cd631f8ed218a54d3d233c
maqsood <designzerosdevelops@gmail.com>
2024-02-20 15:18:47 +0500

    other

--- resources/views/clientpages/contact.blade.php
+++ resources/views/clientpages/contact.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-		<!-- Start Hero Section h -->
+		<!-- Start Hero Section  -->
 			<div class="hero">
 				<div class="container">
 					<div class="row justify-content-between">
