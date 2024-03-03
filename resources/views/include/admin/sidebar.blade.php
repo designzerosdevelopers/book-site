295c78aa74c17a0200134c85cd6e2182415c6a05
maqsood <designzerosdevelops@gmail.com>
2024-03-03 16:19:31 +0500

    changes

--- resources/views/include/admin/sidebar.blade.php
+++ resources/views/include/admin/sidebar.blade.php
@@ -24,7 +24,7 @@
         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <span class="menu-title">Page Edits</span>
           <i class="menu-arrow"></i>
-          <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+          <i class="far fa-edit"></i>
         </a>
         <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
