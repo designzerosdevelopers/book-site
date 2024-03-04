64c2351a30c851c35e11f0ff4fe2c3db429b02df
Saqib Ali <designzerosdevelopers@gmail.com>
2024-03-04 16:31:22 +0500

    erew

--- resources/views/clientpages/productdetail.blade.php
+++ resources/views/clientpages/productdetail.blade.php
@@ -1,35 +1,22 @@
 @extends('layouts.clientside-layout.app')
 
 @section('content')
-<div class="container my-5 py-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-center align-items-center">{{ $product->name }}</div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img src="{{ asset('book_images/'.$product->image ) }}" alt="Product Image" class="img-fluid">
-                        </div>
-                        <div class="col-md-6">
-                            <p>{{ $product->description }}</p>
-                            <div class="d-flex justify-content-center align-items-center">
-                                <h2>${{ $product->price }}</h2>
-                            </div>
-                        
-                            <div class="d-flex justify-content-center align-items-center">
-                                <a href="{{ route('cart', ['id' => $product->id]) }}">
-                                <button class="btn btn-primary btn-sm ml-2">
-                                    ADD TO CART
-                                </button>
-                            </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 product-detail-main-img img-wrap">
+                <img src="{{ asset('book_images/'.$product->image ) }}" alt="Product Image" class="product-image">
             </div>
-            
+            <div class="col-md-6">
+                <h1 class="product-title">{{ $product->name }}</h1>
+                <p class="product-description">{{ $product->description }}</p>
+                
+                <p class="product-price text-success">${{ $product->price }}</p>
+                <div class="d-flex justify-content-center align-items-center">
+                    <a href="{{ route('cart', ['id' => $product->id]) }}">
+                    <button class="btn btn-primary btn-sm ml-2">
+                        ADD TO CART
+                    </button>
+            </div>
         </div>
     </div>
-</div>
 @stop
