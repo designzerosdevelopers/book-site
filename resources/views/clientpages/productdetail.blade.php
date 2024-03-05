e672d4127a1ab0460032667c169e88d8de70f3a0
Saqib Ali <designzerosdevelopers@gmail.com>
2024-03-05 16:57:26 +0500

    w

--- resources/views/clientpages/productdetail.blade.php
+++ resources/views/clientpages/productdetail.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.clientside-layout.app')
 
 @section('content')
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-md-6 product-detail-main-img img-wrap">
-                <img src="{{ asset('book_images/'.$product->image ) }}" alt="Product Image" class="product-image">
-            </div>
-            <div class="col-md-6">
-                <h1 class="product-title">{{ $product->name }}</h1>
-                <p class="product-description">{{ $product->description }}</p>
-                
-                <p class="product-price text-success">${{ $product->price }}</p>
-                <div class="d-flex justify-content-center align-items-center">
-                    <a href="{{ route('cart', ['id' => $product->id]) }}">
-                    <button class="btn btn-primary btn-sm ml-2">
-                        ADD TO CART
-                    </button>
-            </div>
-        </div>
-    </div>
+			   <!-- Start Product Details Section -->
+			   <section class="product-details">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 product-detail-main-img img-wrap">
+							<img src="{{ asset('book_images/'.$product->image ) }}" alt="Product Image" class="product-image">
+						</div>
+						<div class="col-md-6">
+							<h1 class="product-title">{{ $product->name }}</h1>
+							<p class="product-description">
+			
+                            {{ $product->description }}
+							</p>
+
+							<p class="product-price text-success">Price: ${{ $product->price }}</p>
+							 <div class="d-flex justify-content-center align-items-center">
+                                <a href="{{ route('cart', ['id' => $product->id]) }}">
+                                <button class="btn btn-primary btn-sm ml-2">
+                                    ADD TO CART
+                                </button>
+                                </a>
+                            </div>
+							
+						</div>
+					</div>
+				</div>
+			</section>
 @stop
