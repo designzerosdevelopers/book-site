8ad1cd8b1c87c6384217e001a5c139a99bf29a55
maqsood <designzerosdevelops@gmail.com>
2024-03-09 18:39:40 +0500

    changes

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,26 +1,48 @@
+{{-- @extends('layouts.admin-layout.guest')
+@section('content')
+   
+@stop --}}
+
 @extends('layouts.admin-layout.guest')
 @section('content')
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+          <div class="row flex-grow">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-light text-left p-5">
+                <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
+            
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+            
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+            
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 form-control" type="email" name="email" :value="old('email')" required autofocus />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+            
+                    <div class="flex items-center justify-end mt-4 ">
+                        <x-primary-button>
+                            {{ __('Email Password Reset Link') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <!-- container-scroller -->
 @stop
