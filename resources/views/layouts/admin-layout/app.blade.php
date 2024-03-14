87dc84566da49ec208e493fc801c731ea7ce0906
maqsood <designzerosdevelops@gmail.com>
2024-03-14 17:12:21 +0500

    changes

--- resources/views/layouts/admin-layout/app.blade.php
+++ resources/views/layouts/admin-layout/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.base.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/uploads.css')}}">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="{{asset('admin/images/favicon.ico')}}" />
   </head>
@@ -67,6 +68,7 @@
     <script src="{{asset('admin/js/dashboard.js')}}"></script>
     <script src="{{asset('admin/js/todolist.js')}}"></script>
     <script src="{{asset('admin/js/alertcloser.js')}}"></script>
+    <script src="{{asset('admin/js/uploads.js')}}"></script>
     <!-- End custom js for this page -->
 
   </body>
