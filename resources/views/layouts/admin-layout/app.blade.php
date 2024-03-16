1b62dd1282a4184131401881030ee3a63f62895b
maqsood <designzerosdevelops@gmail.com>
2024-03-16 12:37:08 +0500

    c

--- resources/views/layouts/admin-layout/app.blade.php
+++ resources/views/layouts/admin-layout/app.blade.php
@@ -65,11 +65,12 @@
     <!-- endinject -->
 
     <!-- Custom js for this page -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="{{asset('admin/js/dashboard.js')}}"></script>
     <script src="{{asset('admin/js/todolist.js')}}"></script>
     <script src="{{asset('admin/js/alertcloser.js')}}"></script>
     <script src="{{asset('admin/js/uploads.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+   
     <!-- End custom js for this page -->
 
   </body>
