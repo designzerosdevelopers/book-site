1da3c0bd36588cfcc1b2306a6b4c1837ac4fc150
maqsood <designzerosdevelops@gmail.com>
2024-03-16 11:44:21 +0500

    changes

--- resources/views/layouts/admin-layout/app.blade.php
+++ resources/views/layouts/admin-layout/app.blade.php
@@ -71,5 +71,7 @@
     <script src="{{asset('admin/js/uploads.js')}}"></script>
     <!-- End custom js for this page -->
 
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+
   </body>
 </html>
