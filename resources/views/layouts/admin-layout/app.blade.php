8647f779132a6ae549405cf0491aad8f48715beb
maqsood <designzerosdevelops@gmail.com>
2024-03-16 12:40:47 +0500

    c

--- resources/views/layouts/admin-layout/app.blade.php
+++ resources/views/layouts/admin-layout/app.blade.php
@@ -36,7 +36,6 @@
     </div>
     <!-- container-scroller -->
 
-    
     <!-- link for slide up alert status -->
    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -47,8 +46,6 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-
-    
     <script src="{{ asset('admin/ckeditor5/ckeditor.js') }}"></script>
     <script src="{{asset('admin/js/ck.js')}}"></script>
     <!-- plugins:js -->
@@ -65,7 +62,6 @@
     <!-- endinject -->
 
     <!-- Custom js for this page -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="{{asset('admin/js/dashboard.js')}}"></script>
     <script src="{{asset('admin/js/todolist.js')}}"></script>
     <script src="{{asset('admin/js/alertcloser.js')}}"></script>
