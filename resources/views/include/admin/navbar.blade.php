baa08cee401c0c73d78f1ae823d29ae51a351536
maqsood <designzerosdevelops@gmail.com>
2024-03-16 13:05:24 +0500

    c

--- resources/views/include/admin/navbar.blade.php
+++ resources/views/include/admin/navbar.blade.php
@@ -12,7 +12,7 @@
           <div class="input-group">
             <div class="input-group-prepend bg-transparent">
               <i class="input-group-text border-0 mdi mdi-magnify"></i>
-            </div>
+            </div> 
             <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects">
           </div>
         </form>
