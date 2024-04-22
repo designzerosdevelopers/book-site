3e20dd11864474927e1bfc0ba27733fdce51dd7f
saqib <saqibweb52@gmail.com>
2024-04-22 10:43:40 +0500

    db setup

--- database/migrations/2024_02_17_084142_create_categories_table.php
+++ database/migrations/2024_02_17_084142_create_categories_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('category_name');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('categories')) {
+
+            Schema::create('categories', function (Blueprint $table) {
+                $table->id();
+                $table->string('category_name');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
