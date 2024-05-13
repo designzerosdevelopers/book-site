3b44a98195fb1c67be42161360fd8788bfdae979
maqsood <designzerosdevelops@gmail.com>
2024-05-13 17:32:31 +0500

    changes

--- resources/views/adminpages/pages-settings/footer-settings.blade.php
+++ resources/views/adminpages/pages-settings/footer-settings.blade.php
@@ -10,48 +10,72 @@
                     @csrf
                     <input type="hidden" name="comp_name" value="footer">
                     <textarea name="html" rows="10" cols="50" id="editor1">
-                    @if (!empty(App\Helpers\SiteviewHelper::page('footer')))
+                        @if (!empty(App\Helpers\SiteviewHelper::page('footer')))
                             {!! App\Helpers\SiteviewHelper::page('footer')->html !!}
-                            @else
+                        @else
                             No Data
-                            @endif
+                        @endif
                     </textarea>
+                    {{-- <br>//s
+
+                    <div class="form-group">
+                        <label for="colorPicker">Choose footer color:</label>
+                        <input type="color" id="colorPicker" name="footer_bg" value="{{ App\Helpers\SiteviewHelper::getFooterColor()}}">
+                    </div> --}}
                     <button type="submit" class="btn btn-gradient-success my-2">Save</button>
                 </form>
             </div>
         </div>
+    </div>
 
-
-        <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
-        <script>
-            tinymce.init({
-                selector: '#editor1',
-                width: '100%', // Use percentage for responsiveness
-                height: 400, // Adjust height as needed
-                plugins: [
-                    'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
-                    'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
-                    'table', 'emoticons', 'template', 'codesample'
-                ],
-                toolbar: 'undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify |' +
-                    'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
-                    'forecolor backcolor emoticons',
-                menu: {
-                    favs: {
-                        title: 'Menu',
-                        items: 'code visualaid | searchreplace | emoticons'
-                    }
-                },
-                menubar: 'favs file edit view insert format tools table',
-                content_css: [
-                    'clientside/css/style.css',
-                    "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
-                ],
-                protect: {
-                    // Match all content between "<?php" and "?> ?>", greedy matching (.*?)
-                    pattern: /<\?php[\s\S]*?\?>/g
+    <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
+    <script>
+        tinymce.init({
+            selector: '#editor1',
+            width: '100%', // Use percentage for responsiveness
+            height: 400, // Adjust height as needed
+            plugins: [
+                'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+                'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
+                'table', 'emoticons', 'template', 'codesample'
+            ],
+            toolbar: 'undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify |' +
+                'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+                'forecolor backcolor emoticons',
+            // enable title field in the Image dialog
+            image_title: true,
+            // enable automatic uploads of images represented by blob or data URIs
+            automatic_uploads: true,
+            // add custom filepicker only to Image dialog
+            file_picker_types: 'image',
+            menu: {
+                favs: {
+                    title: 'Menu',
+                    items: 'code visualaid | searchreplace | emoticons'
                 }
-                file_picker_callback: function(cb, value, meta) {
+            },
+            menubar: 'favs file edit view insert format tools table',
+            content_css: [
+                'clientside/css/style.css',
+                "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+            ],
+            setup: function(editor) {
+                editor.on('init', function() {
+                    // Get the image elements in the editor content
+                    var images = editor.dom.select('img');
+
+                    // Add event listeners to each image
+                    images.forEach(function(image) {
+                        // Add a 'change' event listener to the title attribute
+                        editor.dom.bind(image, 'change', function() {
+                            // Log the changes to the image title
+                            console.log('Image title changed to:', image.getAttribute('title'));
+                        });
+                    });
+                });
+            },
+            file_picker_callback: function(cb, value, meta) {
                 var input = document.createElement('input');
                 input.setAttribute('type', 'file');
                 input.setAttribute('accept', 'image/*');
@@ -89,8 +113,6 @@
 
                 input.click();
             }
-            });
-        </script>
-
-    @stop
-
+        });
+    </script>
+@stop
